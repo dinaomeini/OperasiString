e5a9e4471c313d9e01ebb09d470075c0f9becac7
Fahdina <Fahdina@LAPTOP-DUGV0GBD>
2018-08-13 21:21:10 -0700

    Bilangan 1-10

---
+++ src/jobsheet11/CobaOperasiString.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet11;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Fahdina
+ */
+public class CobaOperasiString {
+    public static void main(String[] args)
+    {
+        String identitas = "Fahdina Hutami Omeini / X RPL 2 / 11";
+        System.out.println("Identitas : " + identitas);
+    }
+}    
+        
+        
---
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet11;
+import java.util.Scanner;
+/**
+ *
+ * @author Fahdina
+ */
+public class ConvertVocal2Angka {
+    
+    public static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }    
+    public static void main(String[] args){
+        String identitas = "Fahdina Hutami Omeini / X RPL 2 / 11";
+        
+        tampilJudul(identitas);
+        String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
+        tampilkanPerKata(kalimat, convert);
+    }
+        private static String tampilInput() {
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.println("Masukan Kalimat: ");
+            String kalimat = scanner.nextLine();
+            System.out.println("Kalimat Asli : " + kalimat);
+            
+            return kalimat;
+        }   
+            
+            
+            private static String vocal2Angka(String kalimat) {
+                char [][] arConvert = 
+                {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+                
+                kalimat = kalimat.toLowerCase();
+                for(int i=0;i<arConvert.length;i++)
+                    kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+                    
+                    return kalimat;
+                    
+            }
+             
+            private static void tampilkanPerKata(String kalimat, String convert) {
+                    String[]  arrKal = kalimat.split(" ");
+                    String[]  arrCon = convert.split(" ");
+                    
+                    for(int i=0;i<arrKal.length;i++)
+                        System.out.println(arrKal[i] + " => " + arrCon[i]);
+                    
+                    
+            }        
+            private static void tampilkanHasil(String convert) {
+                System.out.println("Kalimat Alay Angka : " + convert);
+                
+                tampilkanHasil(convert);
+            }        
+                }
+                  
+        
+    
+
