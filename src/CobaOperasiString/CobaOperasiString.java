c97f3dbbcd7a5c570d6c46e728b4d327a8d13967
Fahdina <Fahdina@LAPTOP-DUGV0GBD>
2018-08-14 04:23:52 -0700

    Coba method format

--- src/CobaOperasiString/CobaOperasiString.java
+++ src/CobaOperasiString/CobaOperasiString.java
@@ -5,6 +5,8 @@
  */
 package CobaOperasiString;
 
+import java.util.Locale;
+
 /**
  *
  * @author Fahdina
@@ -82,6 +84,12 @@ public class CobaOperasiString {
         System.out.println("pecah berdasarkan huruf 'r' di varable tclean : ");
         for(int i=0;i<arrT.length;i++)
             System.out.println(arrTr[i]);
+        
+double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"),
+                "Uang saya Rp %,.2f", d));
 
 
     }
