03ea524c2db164d5e48b500480c6245f0ead1496
Fahdina <Fahdina@LAPTOP-DUGV0GBD>
2018-08-14 07:15:09 -0700

    Initial Commit

--- src/jobsheet11/ConvertVocal2Angka.java
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -15,54 +15,15 @@ public class ConvertVocal2Angka {
         System.out.println("Identitas : " + identitas);
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
-    }    
+    }
     public static void main(String[] args){
         String identitas = "Fahdina Hutami Omeini / X RPL 2 / 11";
         
         tampilJudul(identitas);
-        String kalimat = tampilInput();
-        String convert = vocal2Angka(kalimat);
-        tampilkanPerKata(kalimat, convert);
-    }
-        private static String tampilInput() {
-            Scanner scanner = new Scanner(System.in);
-            
-            System.out.println("Masukan Kalimat: ");
-            String kalimat = scanner.nextLine();
-            System.out.println("Kalimat Asli : " + kalimat);
-            
-            return kalimat;
-        }   
-            
-            
-            private static String vocal2Angka(String kalimat) {
-                char [][] arConvert = 
-                {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
-                
-                kalimat = kalimat.toLowerCase();
-                for(int i=0;i<arConvert.length;i++)
-                    kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
-                    
-                    return kalimat;
-                    
-            }
-             
-            private static void tampilkanPerKata(String kalimat, String convert) {
-                    String[]  arrKal = kalimat.split(" ");
-                    String[]  arrCon = convert.split(" ");
-                    
-                    for(int i=0;i<arrKal.length;i++)
-                        System.out.println(arrKal[i] + " => " + arrCon[i]);
-                    
-                    
-            }        
-            private static void tampilkanHasil(String convert) {
-                System.out.println("Kalimat Alay Angka : " + convert);
-                
-                tampilkanHasil(convert);
-            }        
-                }
-                  
+    }    
+}    
+           
+
         
     
 
