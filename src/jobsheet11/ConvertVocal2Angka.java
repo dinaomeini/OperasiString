bf629561670f93ef6a8829fd77a770eb5a1596dc
Fahdina <Fahdina@LAPTOP-DUGV0GBD>
2018-08-15 09:31:19 +0700

    Tambah Tampil Per Kata

--- src/jobsheet11/ConvertVocal2Angka.java
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -22,6 +22,7 @@ public class ConvertVocal2Angka {
         tampilJudul(identitas);
         String kalimat = tampilInput();
         String convert = vocal2Angka(kalimat);
+        tampilkanPerKata(kalimat, convert);
     }
         
         private static String tampilInput() {
@@ -44,6 +45,18 @@ public class ConvertVocal2Angka {
         }
                     
                 return kalimat;
+                
+        }
+        private static void tampilkanPerKata(String kalimat, String convert) {
+                    String[]  arrKal = kalimat.split(" ");
+                    String[]  arrCon = convert.split(" ");
+                    
+                    for(int i=0;i<arrKal.length;i++)
+                        System.out.println(arrKal[i] + " => " + arrCon[i]);
+                    
+                    
+            }        
+
                     
             }
 
